50f10b9932283a6917a15dcf1f37d1b2c049e29b
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-09-09 08:43:43 -0500

    Added repository implementation

---
+++ lib/infraestructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,17 @@
+
+import 'package:cimenapedia/domain/datasources/movies_datasource.dart';
+import 'package:cimenapedia/domain/entities/movie.dart';
+import 'package:cimenapedia/domain/repositories/movies_repository.dart';
+
+
+class MovieRepositoryImpl extends MoviesRepository {
+
+  final MoviesDatasource datasource;
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+
+}
